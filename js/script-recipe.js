853d68241ea73a83be914d1101e4899be9877130
RoxanaCS <roxale.cares@gmail.com>
2018-01-08 20:01:33 -0300

    etapa 7

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,49 +1,5 @@
 $(document).ready( function(){
-
-	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
-	renderHighlightedRecipes(recipesArray);
-
+console.log("hola  Mundo");
 });
 //ocultando menu
 $('.js-menu').hide();
-/*
-* Función que se encarga de pintar TODAS las recetas que tengan
-* marcado el atributo "highlighted" como TRUE
-*/
-function renderHighlightedRecipes(recipesArray) {
-	console.log('Recipes: ', recipesArray);
-  for (i=0; i<recipesArray.length; i++){
-    if(recipesArray[i].highlighted = true){
-      //console.log("esto funciona");
-    } renderRecipe(recipesArray[i]);
-    //console.log(recipe);
-  }
-}
-
-/*
-* Función que se encarga de pintar UNA recetas que tenga
-* marcado el atributo "highlighted" como TRUE
-* Aqui se tiene que crear el HTML que esta en el
-* archivo "templates/templates-recipe.html"
-*/
-function renderRecipe(recipe) {
-	console.log('Voy a pintar la receta: ', recipe);
-}
-
-
-
-/*
-* Función que se encarga de pintar todas las actividades
-*/
-function renderActivities(activitiesArray) {
-	console.log('Activities: ', activitiesArray);
-}
-
-/*
-* Función que se encarga de pintar una actividad
-* Aqui se tiene que crear el HTML que esta en el
-* archivo "templates/templates-activity.html"
-*/
-function renderActivity(recipe) {
-
-}
