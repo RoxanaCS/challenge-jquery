ba139429ea704b3e7c066f3aa5d5e1df874126d2
RoxanaCS <roxale.cares@gmail.com>
2018-01-08 20:25:06 -0300

    etapa 9

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -8,8 +8,12 @@ $('.js-menu').hide();
 
 $('.js-show-recipe').click(function(){
 	$('.page').removeClass('make');
+	$('.js-show-recipe').addClass('active');
+	$('.js-show-make').removeClass('active');
 });
 
 $('.js-show-make').click(function(){
 	$('.page').addClass('make');
+	$('.js-show-make').addClass('active');
+	$('.js-show-recipe').removeClass('active');
 });
